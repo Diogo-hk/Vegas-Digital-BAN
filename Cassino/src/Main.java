ff03dfd6ee070f8987a1d74cfba6af3a9dedf997
Diogo <diogocarvalhohk@gmail.com>
2025-11-30 23:01:29 -0300

    Alterando local do arquivo

--- Cassino/src/Main.java
+++ Cassino/src/Main.java
@@ -1,5 +1,19 @@
+import controller.LoginController; // Importe o controller
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        LoginController controller = new LoginController();
+
+        System.out.println("=== SISTEMA DE CASSINO ===");
+        System.out.print("Digite seu Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Digite sua Senha: ");
+        String password = scanner.nextLine();
+        controller.entrarNoSistema(email, password);
+
+        scanner.close();
     }
 }
