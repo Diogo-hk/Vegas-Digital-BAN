43dcdd285e2378361b304024a1b1e5c700047acc
Diogo Silva de Carvalho <147107925+Diogo-hk@users.noreply.github.com>
2025-11-30 23:37:31 -0300

    RODA DESGRAÇAAAAAAAAAA

--- Cassino/src/Conexao/Conexao.java
+++ Cassino/src/Conexao/Conexao.java
@@ -14,8 +14,8 @@ public class Conexao {
     public Conexao() {
         String driver = "org.postgresql.Driver";
         String user = "postgres";
-        String senha = "postgres";
-            String url = "jdbc:postgresql://localhost:5432/Cassino";
+        String senha = "diogo4545";
+        String url = "jdbc:postgresql://localhost:5432/TrabalhoBAN";
 
         try {
             Class.forName(driver);
